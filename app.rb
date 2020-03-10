dca966325851bd4bfcf75fd18588f5a457156d1a
dinnosaur <dinnoroni@gmail.com>
2020-03-10 16:57:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,30 @@
 require 'sinatra/base'
 
+
 class App < Sinatra::Base
+    
+    get '/newteam' do 
+        erb :newteam
+    end
+
+    post '/team' do
+      
+        @team_name = params[:name]
+        @coach = params[:coach]
+        @pg = params[:pg]
+        @sg = params[:sg]
+        @pf = params[:pf]
+        @sf = params[:sf]
+        @c = params[:c]
+        @id = params[:id]
+        puts @id
+        
+        erb :team
+    end 
+
+
+
+    
 
 
 end
